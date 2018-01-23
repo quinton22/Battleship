0a0fc6d06e56a1832f672a2c814eabaaa2fc1979
quinton22 <quintonhoffman22@gmail.com>
2018-01-22 18:33:39 -0600

    Changed board to a <table>

--- content.js
+++ content.js
@@ -1,8 +1,8 @@
 // Variables to change how the game is played
-var shipSizes = [5, 4, 3, 3, 2]; // size of each ship
+const SHIP_SIZES = [5, 4, 3, 3, 2]; // size of each ship
+const COLUMN_LETTERS = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S",
+   "T", "U", "V", "W", "X", "Y", "Z"];
 
-var canvas = document.querySelector("canvas");
-var c = canvas.getContext("2d");
 var startingPositions = {
    carrier: [0, 0],
    battleship: [0, 0],
@@ -12,62 +12,123 @@ var startingPositions = {
 }
 
 class Ship {
-   constructor(x, y, height, width) {
-      this.x = x;
-      this.y = y;
-      this.height = height;
-      this.width = width;
-      this.orientation = "v";
-      this.hit = false;
+   constructor(x, y, height, width, length) {
+      this.x = x; // x position (px)
+      this.y = y; // y position (px)
+      this.height = height; // height (px)
+      this.width = width; // width (px)
+      this.length = length // length of ship (number of blocks)
+      this.orientation = "v"; // v = vertical, h = horizontal
+      this.hit = false; // hit or miss
+   }
+
+   // draws a ship which is a gray rectangle
+   draw() {
+      let ship = document.createElement("div");
+      ship.style.top = this.y.toString() + "px";
+      ship.style.left = this.x.toString() + "px";
+      ship.setAttribute("height", this.height);
+      ship.setAttribute("width", this.width);
+      ship.className = "ship";
+
+      return ship;
    }
 }
 
 class Board {
    constructor(x, y, height, width) {
-      this.x = x;
-      this.y = y;
-      this.height = height;
-      this.width = width;
-      this.size = 10;
-      this.carrier = new Ship(this.x - 100, this.y, shipSizes[0] * this.height/(this.size + 1), this.width/(this.size + 1));
-      this.battleship = new Ship(this.x - 100, this.y, shipSizes[1] * this.height/(this.size + 1), this.width/(this.size + 1));
-      this.cruiser = new Ship(this.x - 100, this.y, shipSizes[2] * this.height/(this.size + 1), this.width/(this.size + 1));
-      this.submarine = new Ship(this.x - 100, this.y, shipSizes[3] * this.height/(this.size + 1), this.width/(this.size + 1));
-      this.destroyer = new Ship(this.x - 100, this.y, shipSizes[4] * this.height/(this.size + 1), this.width/(this.size + 1));
+      this.x = x; // x position (px)
+      this.y = y; // y position (px)
+      this.height = height; // height (px)
+      this.width = width; // width (px)
+      this.size = 10; // Playable grid size
 
+      // Creates array of Ship objects
+      this.ships = [];
+      let _this = this; // allows this to work in foreach loop
+      SHIP_SIZES.forEach(function(length) {
+         _this.ships.push(new Ship(_this.x-100, _this.y, length * _this.height/(_this.size + 1), _this.width/(_this.size + 1), length));
+      });
    }
 
    draw() {
-      
+      // Create the board
+      let tableDiv = document.createElement("div");
+      let table = document.createElement("table");
+      tableDiv.appendChild(table);
+      for(let q = 0; q <= this.size; ++q) {
+         let tr = document.createElement("tr");
+         let td;
+         for(let r = 0; r <= this.size; ++r) {
+            if (q === 0 || r === 0) {
+               td = document.createElement("th");
+               if (q === 0 && r !== 0) {
+
+                  // Puts column letters at top of table, if longer than 26 then uses A1, B1, ... etc
+                  if (q < COLUMN_LETTERS.length) {
+                     td.innerHTML = COLUMN_LETTERS[r - 1];
+                  } else {
+                     td.innerHTML = COLUMN_LETTERS[(r - 1) % this.size] + Math.floor((r - 1) / this.size).toString();
+                  }
+               // Puts numbers on side of rows
+               } else if (r === 0 && q !== 0) {
+                  td.innerHTML = q.toString();
+               }
+            } else {
+               td = document.createElement("td");
+               td.ID = COLUMN_LETTERS[(r - 1) % this.size] + Math.floor((r - 1) / this.size).toString() + "_" + q.toString();
+            }
+            td.setAttribute("width", this.width / (this.size + 1));
+            tr.appendChild(td);
+            tr.setAttribute("height", this.height / (this.size + 1));
+         }
+         tr.style.width = this.width;
+         table.appendChild(tr);
+      }
+      table.setAttribute("height", this.height);
+      table.setAttribute("width", this.width);
+      tableDiv.appendChild(table);
+      tableDiv.className = "table-div";
+      tableDiv.style.top = this.y.toString() + "px";
+      tableDiv.style.left = this.x.toString() + "px";
+      console.log();
+
+      // Create the grid lines on the board
+      let blockSize = [this.width/(this.size + 1), this.height/(this.size + 1)]; // width of one block and height of one block
+
+      this.ships.forEach(function (ship) {
+         //tableDiv.appendChild(ship.draw());
+      });
+      document.getElementById("board-container").appendChild(tableDiv);
    }
+
+
 }
 
-
-window.addEventListener("resize", function() {
-   canvas.width = window.innerWidth;
-   canvas.height = window.innerHeight;
-})
-
 window.onload = function() {
-   canvas.width = window.innerWidth;
-   canvas.height = window.innerHeight;
-   c.fillStyle = document.body.style.background;
 
-   document.getElementById("onePlayer").addEventListener("click", function () {
+   // click event to one player button
+   document.getElementById("one-player-btn").addEventListener("click", function () {
       console.log("Init 1 player mode");
       hideTitle(1);
    });
 
-   document.getElementById("twoPlayer").addEventListener("click", function() {
+   // click event to two player button
+   document.getElementById("two-player-btn").addEventListener("click", function() {
       console.log("Init 2 player mode");
       hideTitle(2);
+   });
+
+   // click event on instructions button
+   document.getElementById("instructions-btn").addEventListener("click", function() {
+      console.log("Instructions Page")
    });
 };
 
 // Hides the titleDiv and calls mode function to initiate game
 function hideTitle(mode) {
    document.getElementById("titleDiv").style.display = "none";
-   document.querySelector("canvas").style.display = "block";
+   document.getElementById("board-container").style.display = "block";
    if (mode === 1) {
       initOnePlayer();
    } else {
@@ -77,80 +138,23 @@ function hideTitle(mode) {
 
 // Starts the one player mode
 function initOnePlayer() {
-   document.getElementById("onePlayerMode").style.display = "block";
-   canvas.style.display = "block";
+   document.getElementById("one-player-mode").style.display = "block";
 }
 
 // Starts the two player mode
 function initTwoPlayer() {
-   let board1 = new Board()
-   document.getElementById("twoPlayerMode").style.display = "block";
-   canvas.style.display = "block";
-   c.strokeRect(boardPositio, boardPosition[1]);
+   let boardHeight = document.getElementById("board-container").offsetWidth;
+
+   // Sets board height and width depending on the page size;
+
+   // creates board
+   let board1 = new Board(window.innerWidth/4, boardHeight/2, boardHeight, boardHeight);
 
 
-// c = canvas.getContext('2d')
-// c.fillStyle
-// c.fillRect(x,y, width, height)
+   document.getElementById("two-player-mode").style.display = "block";
+   console.log(document.getElementById("board-container").offsetWidth);
+   setTimeout(function () {
+      board1.draw(); // draws board
+   }, 100);
 
-// Line:
-// c.beginPath()
-// c.moveTo(x,y)
-// c.lineTo(x,y)
-// c.strokeStyle =
-// c.stroke
-
-// Arc / circle
-// c.beginPath()
-// c.arc(x, y, r, startAngle, endAngle, drawCounterClockwise)
-// c.stroke()
-
-// var xVal = 100;
-// function animate() {
-//    requestAnimationFrame(animate);
-//
-//    c.beginPath();
-//    c.arc(x, 100, 50, 0, Math.PI * 2, false);
-//    c.stroke();
-//
-//    x += 1;
-//
-// }
-//
-// animate()
-
-// //start with only the mousedown event attached
-// canvas.addEventListener("mousedown",mousedown);
-//
-// //and some vars to track the dragged item
-// var dragIdx = -1;
-// var dragOffsetX, dragOffsetY;
-//
-// function mousedown(e){
-//     //...calc coords into mouseX, mouseY
-//     for(i=circArray.length; i>=0; i--){ //loop in reverse draw order
-//         dx = mouseX - circArray[i].x;
-//         dy = mouseY - circArray[i].y;
-//         if (Math.sqrt((dx*dx) + (dy*dy)) < circArray[i].r) {
-//             //we've hit an item
-//             dragIdx = i; //store the item being dragged
-//             dragOffsetX = dx; //store offsets so item doesn't 'jump'
-//             dragOffsetY = dy;
-//             canvas.addEventListener("mousemove",mousemove); //start dragging
-//             canvas.addEventListener("mouseup",mouseup);
-//             return;
-//         }
-//     }
-// }
-//
-// function mousemove(e) {
-//      //...calc coords
-//      circArray[dragIdx].x = mouseX + dragOffsetX; //drag your item
-//      circArray[dragIdx].y = mouseY + dragOffsetY;
-//      //...repaint();
-// }
-//
-// function mouseup(e) {
-//      dragIdx = -1; //reset for next mousedown
-//     canvas.removeListener(.... //remove the move/up events when done
 }
